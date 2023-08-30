ecb7cca16737462108bca87e303a492d73b3e3ae
wajihalam <wajih.alam@usask.ca>
2023-08-30 19:06:29 -0400

    feat: implemented auth feature

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -11,9 +11,9 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
 import compression from 'compression';
-import { config } from './config';
-import applicationRoutes from './routes';
-import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -112,7 +112,7 @@ export class ChattyServer {
     });
   }
 
-   private socketIOConnections(io: Server): void {
+  private socketIOConnections(io: Server): void {
     // const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
     // const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
     // const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
@@ -125,5 +125,5 @@ export class ChattyServer {
     // chatSocketHandler.listen();
     // notificationSocketHandler.listen(io);
     // imageSocketHandler.listen(io);
-   }
+  }
 }
